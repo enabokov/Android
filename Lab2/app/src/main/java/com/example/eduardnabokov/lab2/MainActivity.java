ce413a4975d9ce0c0307413ab9305b7213ac3f47
enabokov <edwardnabokov@gmail.com>
2018-05-26 09:34:11 +0300

    Implement Lab2

--- Lab2/app/src/main/java/com/example/eduardnabokov/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/eduardnabokov/lab2/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,17 +21,61 @@ public class MainActivity extends AppCompatActivity {
         addSubmitButtonListener();
     }
 
-    private void countriesCheckBoxes() {
-        Button button = findViewById(R.id.checkbox_china);
-        System.out.println("China button is " + button.isSelected());
+    private StringBuilder countriesCheckBoxes() {
+        List<CheckBox> boxes = new ArrayList<>();
+        boxes.add((CheckBox) findViewById(R.id.checkbox_china));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_france));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_italy));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_netherlands));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_russia));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_spain));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_ukraine));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_usa));
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("Countries: ");
+        for(CheckBox box : boxes) {
+            if (box.isChecked())
+                result.append(box.getText() + " ");
+        }
+
+        result.append("\n");
+
+        return result;
+    }
+
+    private StringBuilder pricesCheckBoxes() {
+        List<CheckBox> boxes = new ArrayList<>();
+        boxes.add((CheckBox) findViewById(R.id.checkbox_500));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_500_1000));
+        boxes.add((CheckBox) findViewById(R.id.checkbox_1000));
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("Prices: ");
+        for(CheckBox box: boxes) {
+            if (box.isChecked())
+                result.append(box.getText() + " ");
+        }
+
+        result.append("\n");
+
+        return result;
     }
 
     private void addSubmitButtonListener() {
         final Button button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                countriesCheckBoxes();
-                Toast.makeText(MainActivity.this, "Submit", Toast.LENGTH_LONG).show();
+                StringBuilder res = new StringBuilder();
+                StringBuilder res1 = countriesCheckBoxes();
+                StringBuilder res2 = pricesCheckBoxes();
+
+                res.append(res1);
+                res.append(res2);
+
+                Toast.makeText(MainActivity.this, res, Toast.LENGTH_LONG).show();
             }
         });
     }
